c48b33cf5dfd0eb34e7a7980eeb1c5cef4ef9b21
543807939@qq.com <543807939@qq.com>
2022-07-11 00:24:44 +0800

    study

---
+++ 02_javascript/01_base/01_js的基本语法/js/01_bing.js
@@ -0,0 +1,13 @@
+const bingEle = document.querySelector('#bing')
+bingEle.addEventListener('click', e => {
+    console.log(e);
+})
+
+/**
+ * 一个普通的测试函数
+ * @param {string} name 姓名
+ * @param {number} age 年龄
+ */
+function foo(name, age) {
+
+}
